cd91875ee6ea612fce6b8c70397dbca95da83331
norman1208 <achmadnorman1208@gmail.com>
2024-10-09 10:40:01 +0700

    feat emoji app: all of basic design for main page

---
+++ emoji-app-nextjs-w3d13dti/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+
+const Header: FC = () => {
+    return (
+        <header className="relative top-0 left-0 py-6 px-10 z-[2]">
+            <h1 className="font-sans text-[#F3CF56] border-1 border-[#4A7582]">EMOJILOGY</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ emoji-app-nextjs-w3d13dti/src/components/emoji.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+
+
+const Emoji: FC = () => {
+    return (
+        <div className=" pl-5 flex w-[382px] justify-center items-center pb-10">
+            <div className="bg-[#F8DD84] h-[841px]">
+                <div className="pt-[170px] flex justify-center items-center">
+                    <h1 className="text-center font-normal text-[#4C9BB9] w-[340px] text-[40px] justify-center items-center">What&apos;s your <span className="text-[#1C5469]">emoji</span> today?</h1>
+                </div>
+                <div className="pt-[100px] flex flex-col justify-center items-center">
+                    <span className="text-[#4A7582] text-[20px] font-sans">Click it!</span>
+                    <div>
+                        emoticon
+                    </div>
+                </div>
+
+                <div className="pt-[200px] flex justify-center">
+                    <p className="text-[#4A7582] text-center w-[184px]">You’re sad and you know it. just give up, don’t try.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Emoji;
