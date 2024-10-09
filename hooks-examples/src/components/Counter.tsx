cddc92c088057fd39961a216a33e753982edba49
norman1208 <achmadnorman1208@gmail.com>
2024-10-09 09:31:57 +0700

    first commit for emoji-app project

--- hooks-examples/src/components/Counter.tsx
+++ hooks-examples/src/components/Counter.tsx
@@ -18,7 +18,7 @@ const Counter: FC    = () => {
     return (
         <div className="flex">
             <h1>Counter</h1>
-            <p>count: <span className="font-bold text-red-600">{count}</span></p>
+            <p>count: <span className="font-bold text-red-600s">{count}</span></p>
             <button onClick={handleIncrement} className="px-4 py-2 border-double">Increment</button>
             <button onClick={handleDecrement} className="px-4 py-2 border-double">Decrement</button>
         </div>
